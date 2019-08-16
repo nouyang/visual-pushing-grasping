fixed issue with checker offset (flip x sign)

calibrate.py
@@ -11,8 +11,8 @@ from scipy import optimize
 from mpl_toolkits.mplot3d import Axes3D
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
-# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # User options (change me)
 # --------------- Setup options ---------------
@@ -43,18 +43,18 @@ workspace_limits = np.asarray(  # smaller true-ish (63 pts)
 # direction..
 """
 workspace_limits = np.asarray(
-    [[-0.500, -0.400], [-0.000, 0.100], [-0.200, -0.100]])
+    [[-0.650, -0.400], [-0.100, 0.100], [-0.300, -0.150]])
 
 # calib_grid_step = 0.05
 #calib_grid_step = 0.15
 
-calib_grid_step = 0.05
+calib_grid_step = 0.1
 
 
 # checkerboard_offset_from_tool = [0, -0.13, 0.02] # ORIGINAL
 
 # NOTE: measured
-checkerboard_offset_from_tool = [0.090, 0.000, 0.020]  # gripper is 2cm high
+checkerboard_offset_from_tool = [-0.090, 0.000, 0.020]  # gripper is 2cm high
 
 # Original
 # tool_orientation = [-np.pi/2, 0, 0]
@@ -118,6 +118,8 @@ print('Collecting data...')
 start = time.time()
 print('num calib pts', num_calib_grid_pts)
 
+time.sleep(1)
+
 for calib_pt_idx in range(num_calib_grid_pts):
     tool_position = calib_grid_pts[calib_pt_idx, :]
 
@@ -163,12 +165,12 @@ for calib_pt_idx in range(num_calib_grid_pts):
         # TODO: Is this offset in the right direction?
         # TODO: Is this offset from the gripper jaws or from the tool of the
         # robot?
-        tool_position = tool_position + checkerboard_offset_from_tool
+        checker_position = tool_position + checkerboard_offset_from_tool
 
-        print('I measured (calculated)', tool_position)
+        print('I measured (calculated)', checker_position)
         print('I observed (realsense)', checkerboard_x, checkerboard_y,
               checkerboard_z)
-        measured_pts.append(tool_position)
+        measured_pts.append(checker_position)
         observed_pix.append(checkerboard_pix)
 
         # Draw and display the corners

heightmap_test.py
@@ -20,23 +20,26 @@ camera = Camera()
 cam_pose = np.loadtxt("real/camera_pose.txt", delimiter=" ")
 cam_depth_scale = np.loadtxt(
     "real/camera_depth_scale.txt", delimiter=" ")
+cam_intrinsics = camera.intrinsics
+print(cam_depth_scale, cam_intrinsics)
 
 workspace_limits = np.asarray(
-    [[-0.660, -0.310], [-0.115, 0.235], [-0.400, -0.310]])  # with gripper up
-# [[-0.770, -0.420], [-0.125, 0.225], [-0.400, -0.310]]) # with gripper down
+    # [[-0.710, -0.360], [-0.115, 0.235], [-0.400, -0.310]])  # with gripper horizontal
+    [[-0.700, -0.350], [-0.125, 0.225], [-0.420, -0.300]])  # with gripper vertical
 
-cam_intrinsics = np.array([[920.723,   0., 653.2795],
-                           [0., 920.02124, 362.55386],
-                           [0.,   0.,   1.]])
+# Cam intrinsices are sent by camera directly
+# cam_intrinsics = np.array([[920.723,   0., 653.2795],
+# [0., 920.02124, 362.55386],
+# [0.,   0.,   1.]])
 
 # color_img = cv2.imread('color.png', 0)
 # depth_img = cv2.imread('depth.png', 0)
-depth_scale = np.array(0.99453125)
 color_img, depth_img = camera.get_data()
-depth_img = depth_img * depth_scale
-# color_img = cv2.resize(color_img, (0, 0), fx=0.2, fy=0.7)
-# depth_img = cv2.resize(depth_img, (0, 0), fx=0.2, fy=0.7)
-heightmap_resolution = 0.001
+depth_img = depth_img * cam_depth_scale
+k = 0.64
+color_img = cv2.resize(color_img, (0, 0), fx=k, fy=k)
+depth_img = cv2.resize(depth_img, (0, 0), fx=k, fy=k)
+heightmap_resolution = 0.001*(1./k)
 # depth_img = depth_img.astype(float) * depth_scale
 
 color_heightmap, depth_heightmap = utils.get_heightmap(