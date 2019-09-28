fixed specific rotation

models.py
@@ -69,8 +69,14 @@ class reinforcement_net(nn.Module):
         interm_feat = []
         physics_prediction = [1.0] * input_color_data.shape[0]
 
-      # Apply rotations to images
+        # Apply rotations to images
         for rotate_idx in range(self.num_rotations):
+
+            # do only one rotation
+            if specific_rotation is not None and 0 <= specific_rotation < self.num_rotations:
+                if rotate_idx != specific_rotation:
+                    continue
+
             rotate_theta = np.radians(rotate_idx*(360/self.num_rotations))
 
             # Compute sample grid for rotation BEFORE neural network
@@ -126,7 +132,7 @@ class reinforcement_net(nn.Module):
             #with torch.no_grad():
             self.grasp_output = self.all_nets.grasp_net(self.visual_features_with_physics_channel)
 
-            print(torch.cuda.memory_allocated() / 1000000000, "GB")
+            #print(torch.cuda.memory_allocated() / 1000000000, "GB")
 
             # interm_feat.append(self.visual_features)
 

playground/run_models_new.py
@@ -24,13 +24,25 @@ def main(args):
 
     if args.no_grad:
         with torch.no_grad():
-            outputs_t = net.forward(input_color_data_t, input_depth_data_t, physics_prediction)
+            outputs_t = net.forward(
+                input_color_data_t, input_depth_data_t, physics_prediction, specific_rotation=args.specific_rotation
+            )
     else:
-        outputs_t = net.forward(input_color_data_t, input_depth_data_t, physics_prediction)
+        outputs_t = net.forward(
+            input_color_data_t, input_depth_data_t, physics_prediction, specific_rotation=args.specific_rotation
+        )
 
     outputs = [ft.detach().cpu().numpy() for ft in outputs_t]
 
     for rot_idx in range(num_rotations):
+
+        if args.specific_rotation is not None:
+            # hacky way of showing only one image
+            if rot_idx != args.specific_rotation:
+                continue
+            else:
+                rot_idx = 0
+
         plt.subplot(4, 4, rot_idx + 1)
 
         img = outputs[rot_idx][0, 0, :, :]
@@ -44,5 +56,6 @@ def main(args):
 parser = argparse.ArgumentParser()
 parser.add_argument("--device", default="cuda:0")
 parser.add_argument("--no-grad", default=False, action="store_true")
+parser.add_argument("--specific-rotation", type=int, default=None)
 parsed = parser.parse_args()
 main(parsed)