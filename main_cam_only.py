3368eaf2741e1a7252f372e8268974cd895d946f
N Ouyang <nouyang@alum.mit.edu>
2019-12-25 06:50:32 -0500

    run realsense cam to debug utils.py heightmap conversion, with robot off

---
+++ main_cam_only.py
@@ -0,0 +1,39 @@
+import utils
+from real.camera import Camera
+import matplotlib.pyplot as plt
+
+def main():
+    camera = Camera()
+    cam_intrinsics = camera.intrinsics
+
+    # Load camera pose (from running calibrate.py), intrinsics and depth scale
+    # NOTE: Is this independent of where the camera is?
+    cam_pose = np.loadtxt('real/camera_pose.txt', delimiter=' ')
+    cam_depth_scale = np.loadtxt(
+     'real/camera_depth_scale.txt', delimiter=' ')
+
+
+    workspace_limits = np.asarray(
+            [[0.350, 0.650], [-0.250, 0.180], [0.080, 0.350]])
+    heightmap_resolution =0.002 # DEFAULT  # help='meters per pixel of heightmap')
+
+    color_img, depth_img = camera.get_data()
+    # Apply depth scale from calibration
+    depth_img = depth_img * cam_depth_scale
+
+    # Get heightmap from RGB-D image (by re-projecting 3D point cloud)
+    color_heightmap, depth_heightmap = utils.get_heightmap(
+        color_img, depth_img, cam_intrinsics, cam_pose, workspace_limits, heightmap_resolution)
+
+    valid_depth_heightmap = depth_heightmap.copy()
+    valid_depth_heightmap[np.isnan(valid_depth_heightmap)] = 0
+
+    # Save RGB-D images and RGB-D heightmaps
+    # save_images(trainer.iteration, color_img, depth_img, '0')
+    # save_heightmaps(
+        # trainer.iteration, color_heightmap, valid_depth_heightmap, '0')
+    plt.figure()
+    plt.show(color_img)
+
+if __name__ == '__main__':
+    main()
